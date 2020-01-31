eec29d0698f59e1ae67a3deec9406d2749b8359f
EdwinBoersema <edwinboersema@gmail.com>
2020-01-31 11:25:03 +0100

    Add initial files and directories.

---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Outdoor Gear Inventory</title>
+
+    <link rel="stylesheet" href="public/bootstrap.min.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <nav class="nav bg-dark">
+            <a class="nav-link" href="index.php">Home</a>
+            <a class="nav-link" href="add.php">Add</a>
+        </nav>
